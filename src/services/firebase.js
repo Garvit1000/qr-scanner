abcc2f4ccbf5e03c2aee9aea8594abd55abd29fc
KARTIK PAREEK <115743119+KARTIK614@users.noreply.github.com>
2025-09-20 10:49:27 +0530

    Update firebase.js
    updating the configuration.

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCzFbDhwQvcNXI-XCMGgzUCOvuGUfpvkDg",
-  authDomain: "scanner-90445.firebaseapp.com",
-  projectId: "scanner-90445",
-  storageBucket: "scanner-90445.firebasestorage.app",
-  messagingSenderId: "1078770639893",
-  appId: "1:1078770639893:web:b80abb01b910c304b95a1c",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
